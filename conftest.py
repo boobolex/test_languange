99988f510100f196a99d208cd418a12fd3a298ec
boobolex <vinta1971@yandex.ru>
2021-01-03 15:16:16 +0400

    reduct test_main_page.py

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default="en",
                      help="Choose language")
 
 

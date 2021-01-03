f7e28c8f0aa841d7d66a27d792ed1cb889b558c1
boobolex <vinta1971@yandex.ru>
2021-01-03 21:29:06 +0400

    add assert

--- test_items.py
+++
@@ -1,3 +0,0 @@
-def test_guest_should_see_login_link(browser):
-    browser.get(' http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    browser.find_element_by_css_selector("button.btn:nth-child(3)")

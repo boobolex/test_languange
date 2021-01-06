42e4ed1ac6441cd4a51e613a5f58223da9ab3404
boobolex <vinta1971@yandex.ru>
2021-01-06 20:31:01 +0400

    AddToCart

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,14 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocatorss():
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_FORM =  (By.CSS_SELECTOR, "#login_form")   
+
+class ProductPageLocators(object):
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner ")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
